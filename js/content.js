2b3423631297aece33732aaaeebef21992a3efc4
Hugo Marcelo Muriel <marcelo.muriel@syscrunch.com>
2013-07-09 21:23:22 +0200

    multi-intervals playback working on a sample video data. Yet the controls not working to set custom start/end points nor the storage of the video data

--- js/content.js
+++ js/content.js
@@ -1,51 +1,88 @@
 var templates = {
-	
+
 };
 var pageHandler = {
 	youtube : {
-		videoID : (function() {
-			return ( v = /.*v=(.{11})/ig.exec(window.location)) != null ? v.pop() : null;
+		videoMeta : (function(videoID) {
+			id = ( v = /.*v=(.{11})/ig.exec(window.location)) != null ? v.pop() : false;
+			if (id) {
+				// TODO: Retrieve video json metadata from remote storage
+				videoMeta = {
+					videoID : id,
+					duration : 179,
+					intervals : [],
+					tags : [{
+						"15" : "Here she drops the bowl",
+						"55" : "This is funny!"
+					}]
+				};
+				return videoMeta;
+			}
+			return false;
 		})(),
-		loadVideo : function() {
-			// Try to load the video with custom start and end points.
-			setTimeout(function() {
-				player = window['player-api'].children[0];
-				if ( typeof player.loadVideoById === 'function') {
-					player.loadVideoById({
-						'videoId' : pageHandler.youtube.videoID,
-						'startSeconds' : 5,
-						'endSeconds' : player.getDuration() - 5,
-						'suggestedQuality' : 'large'
-					});
+		player : window['player-api'].children[0],
+		intervalIndex : -2,
+		_timer : -1,
+		watchPlayer : function() {
+			if (!this.videoMeta.intervals.length)
+				return;
+			clearInterval(this._timer);
+			checkInterval = function() {
+				yt = pageHandler.youtube;
+				if ( typeof yt.player.loadVideoById === 'function') {
+					//console.log(yt.player.getPlayerState() + "; " + yt.player.getCurrentTime() + ";" + yt.intervalIndex);
+					intervals = yt.videoMeta.intervals;
+					t = yt.player.getCurrentTime();
+					if (yt.intervalIndex == -2 || (t < yt.videoMeta.duration && t >= intervals[yt.intervalIndex + 1])) {
+						// next interval
+						yt.intervalIndex += 2;
+						if (yt.intervalIndex < intervals.length)
+							yt.loadAndPlay(intervals[yt.intervalIndex], intervals[yt.intervalIndex + 1]);
+					}
 				} else {
-					console.log("loadVideoById not yet ready.");
-					this.loadVideo();
+					console.log("YouTube player not yet ready.");
 				}
-			}, 100);
-
+			};
+			checkInterval();
+			this._timer = setInterval(checkInterval, 1000);
+		},
+		loadAndPlay : function(start, end) {
+			player = window['player-api'].children[0];
+			player.loadVideoById({
+				'videoId' : this.videoMeta.videoID,
+				'startSeconds' : start,
+				'endSeconds' : end,
+				'suggestedQuality' : 'large'
+			});
 		},
 		loadControls : function() {
-			controls = document.getElementById('smartplaylists_controls');
-			if (!controls){
-				controls = document.createElement('div');
-				controls.setAttribute('id', 'smartplaylists_controls');
-				controls.setAttribute('class', 'yt-controls player-width');
-				controls.innerHTML = "<h2>Here comes the controls</h2>";
-				document.getElementById('player').appendChild(controls);
+			if (!$('.sp_controls').length) {
+				$('#player').append(new EJS({
+					url : chrome.extension.getURL('js/content.ejs')
+				}).render(this.videoMeta));
+				$('.btn_control').button().click(function(e) {
+					e.preventDefault();
+				})
+				$(function() {
+					$("#range").colResizable({
+						liveDrag : true,
+						draggingClass : "rangeDrag",
+						hoverCursor : "pointer",
+						dragCursor : "e-resize",
+						gripInnerHtml : "<div class='rangeGrip'></div>",
+						onResize : function() {
+						},
+						minWidth : 8
+					});
+				});
 			}
 		}
 	},
 	init : function() {
-		if (this.youtube.videoID) {
-			this.youtube.loadVideo();
+		if (this.youtube.videoMeta) {
 			this.youtube.loadControls();
+			this.youtube.watchPlayer();
 		}
 	}
 }
 pageHandler.init();
-
-console.log("content script");
-script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = chrome.extension.getURL('js/youtube.js');
-document.body.appendChild(script);
