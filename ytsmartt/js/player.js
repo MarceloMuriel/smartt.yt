b1395d6d18029007f0bcc7e763d22a7ca63885f2
H. Marcelo Muriel <marcelomuriel@gmail.com>
2014-10-10 08:40:25 +0200

    Player seekTo function adapted in the case of the flash player to a timeout of 100ms

--- ytsmartt/js/player.js
+++ ytsmartt/js/player.js
@@ -1,46 +1,70 @@
 /**
- * This can be the HTML or Flash player object. YouTube exposes 
- * the same functionality for either of them.
+ * This can be the HTML or Flash player object. YouTube exposes the same
+ * functionality for either of them.
  */
 player = window['player-api'].children[0];
 
-startPlayer = function(p){
+startPlayer = function(p) {
 	/**
-	 * Mute the player to allow the YouTube app to take control over the playback. 
+	 * Mute the player to allow the YouTube app to take control over the
+	 * playback.
 	 */
 	p.pauseVideo();
 	p.mute();
 	/**
-	 * Add all the event listeners that the YouTube app requires to communicate 
+	 * Add all the event listeners that the YouTube app requires to communicate
 	 * with the player.
 	 */
 	document.addEventListener('YT_player_seekTo', function(e) {
-	    p.seekTo(e.detail.seconds, e.detail.allowSeekAhead);
+		console.log('YT_player_seekTo ' + e.detail.seconds);
+		if (typeof (p.seekTo) === 'function')
+			p.seekTo(e.detail.seconds, e.detail.allowSeekAhead);
+		if (player.type == 'application/x-shockwave-flash') {
+			/*
+			 * The player might not respond the first time. Give it another
+			 * change some time after.
+			 */
+			setTimeout(function() {
+				p.seekTo(e.detail.seconds, e.detail.allowSeekAhead)
+			}, 100);
+		} else
+			console.log('seekTo is not a function..');
 	});
 	document.addEventListener('YT_player_playVideo', function(e) {
-	    p.playVideo();
+		console.log('YT_player_playVideo');
+		p.playVideo();
 	});
 	document.addEventListener('YT_player_pauseVideo', function(e) {
-	    p.pauseVideo();
+		console.log('YT_player_pauseVideo');
+		p.pauseVideo();
 	});
 	document.addEventListener('YT_player_nextVideo', function(e) {
-	    p.nextVideo();
+		console.log('YT_player_nextVideo');
+		p.nextVideo();
 	});
 	document.addEventListener('YT_player_unMute', function(e) {
-	    p.unMute();
+		console.log('YT_player_unMute');
+		p.unMute();
 	});
 	document.addEventListener('YT_player_mute', function(e) {
-	    p.mute();
+		console.log('YT_player_mute');
+		p.mute();
 	});
-	updatePlayer = function(p){
-		document.dispatchEvent(new CustomEvent('YT_player_update', {
-			detail: {
-				'currentTime': p.getCurrentTime(),
-				'state': p.getPlayerState(),
-				'videoUrl': p.getVideoUrl(),
-				'playlistNextIndex': (plist = p.getPlaylist())?(p.getPlaylistIndex() + 1) % plist.length:-1,
-				'playlistNextVideo': (plist = p.getPlaylist())?plist[(p.getPlaylistIndex() + 1) % plist.length]:null 
-			}
+	updatePlayer = function(p) {
+		data = {
+			'currentTime' : p.getCurrentTime(),
+			'state' : p.getPlayerState(),
+			'videoUrl' : p.getVideoUrl(),
+			'playlistNextIndex' : (plist = p.getPlaylist()) ? (p
+					.getPlaylistIndex() + 1)
+					% plist.length : -1,
+			'playlistNextVideo' : (plist = p.getPlaylist()) ? plist[(p
+					.getPlaylistIndex() + 1)
+					% plist.length] : null
+		};
+		// console.log(detail);
+		ev = document.dispatchEvent(new CustomEvent('YT_player_update', {
+			"detail" : data
 		}));
 	};
 	interval_id = setInterval(updatePlayer, 1000, p);
@@ -51,28 +75,29 @@ startPlayer = function(p){
 	updatePlayer(p);
 };
 
-if(player && typeof(player.loadVideoById) === 'function'){
+if (player && typeof (player.loadVideoById) === 'function') {
 	startPlayer(player);
-}else{
+} else {
 	console.log('player is not loaded, waiting..');
-	player_load_interval = setInterval(function(){
+	player_load_interval = setInterval(function() {
 		player = window['player-api'].children[0];
-		if(player && typeof(player.loadVideoById) === 'function'){
+		if (player && typeof (player.loadVideoById) === 'function') {
 			clearInterval(player_load_interval);
 			startPlayer(player);
 		}
 	}, 10);
 }
 /**
- * Hook a click handler to all anchors that have an href starting with /watch?v. 
- * This handler should pause the video player to allow a smooth transition of the 
- * next video avoiding playback before the allowed intervals.
+ * Hook a click handler to all anchors that have an href starting with /watch?v.
+ * This handler should pause the video player to allow a smooth transition of
+ * the next video avoiding playback before the allowed intervals.
  */
-for(idx in document.links){
-	link = document.links[idx]; 
-	if(link.href && link.href != location.href + "#" && link.href.indexOf('/watch?v=') > -1){
-		link.onclick = function(){
-			if(player){
+for (idx in document.links) {
+	link = document.links[idx];
+	if (link.href && link.href != location.href + "#"
+			&& link.href.indexOf('/watch?v=') > -1) {
+		link.onclick = function() {
+			if (player) {
 				player.pauseVideo();
 				player.mute();
 			}
